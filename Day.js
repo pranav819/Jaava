08ce0e8e16fd8e668de102034bcdab91b29076a4
Pranav Nair <145040405+pranav819@users.noreply.github.com>
2024-10-18 15:28:34 +0530

    Add files via upload

---
+++ Day.js
@@ -0,0 +1,25 @@
+var day=parseInt(prompt("Enter no. 1-7"))
+switch (day) {
+    case 1:
+        document.write("Sunday");
+        break;
+    case 2:
+        document.write("Monday");
+        break;
+    case 3:
+        document.write("Tuesday");
+        break;
+    case 4:
+        document.write("Wednesday");
+        break;
+    case 5:
+        document.write("Thursday");
+        break;
+    case 6:
+        document.write("Friday");
+        break;
+    case 7:
+        document.write("Saturday");
+        break;
+
+}
